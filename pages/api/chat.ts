893caf1abb6f5d10677e5cfd568a63bca9ed1fc0
voidchef <neelutpal.saha@s.amity.edu>
2024-03-30 15:05:29 +0000

    fix: update temperature and similarity score

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -54,7 +54,7 @@ export default async function handler(
     }); */
 
     const retriever = ScoreThresholdRetriever.fromVectorStore(vectorStore, {
-      minSimilarityScore: 0.1, // Finds results with at least this similarity score
+      minSimilarityScore: 0.8, // Finds results with at least this similarity score
       maxK: 100, // The maximum K value to use. Use it based to your chunk size to make sure you don't run out of tokens
       kIncrement: 2, // How much to increase K by each time. It'll fetch N results, then N + kIncrement, then N + kIncrement * 2, etc.
     });
--- utils/makechain.ts
+++ utils/makechain.ts
@@ -14,8 +14,7 @@ const CONDENSE_TEMPLATE = `Given the following conversation and a follow up ques
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_TEMPLATE = `You are an expert researcher. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+const QA_TEMPLATE = `You are an expert researcher. Use the following pieces of context as a reference to answer the question at the end also feel free to add your own knowledge along with the context.
 If the question is not related to the context or chat history, politely respond that you are tuned to only answer questions that are related to the context.
 
 <context>
@@ -40,7 +39,7 @@ export const makeChain = (retriever: VectorStoreRetriever) => {
   const answerPrompt = ChatPromptTemplate.fromTemplate(QA_TEMPLATE);
 
   const model = new ChatOpenAI({
-    temperature: 0,
+    temperature: 0.5,
     modelName: 'gpt-3.5-turbo',
   });
 
